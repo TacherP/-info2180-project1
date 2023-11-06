2753c1580c28e8313594b8da4a19edcf8b9af2c3
TacherP <montacher.pierre@mymona.uwi.edu>
2023-11-05 23:57:21 -0500

    finalize project

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 /* Add your JavaScript to this file */
+
+document.addEventListener("DOMContentLoaded", function () {
+  const newsletterForm = document.querySelector("form[action='#']");
+  const emailInput = newsletterForm.querySelector("input[name='email']");
+  const messageDiv = document.querySelector(".message");
+
+  newsletterForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the form from actually submitting
+
+    const email = emailInput.value;
+
+    if (email.trim() === "") {
+      // No email address entered
+      messageDiv.textContent = "Please enter a valid email address.";
+    } else {
+      // Email address entered
+      messageDiv.textContent = `Thank you! Your email address ${email} has been added to our mailing list!`;
+    }
+
+    // Clear the input field
+    emailInput.value = "";
+  });
+});
